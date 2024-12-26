9257846a6004d2943fb252de7bd31cf008dc15d8
Rise LLC <rise.llc.ry@gmail.com>
2024-12-27 00:54:52 +0700

    Changed Files' Code

--- curzs.js
+++ curzs.js
@@ -20,6 +20,8 @@ const curzs =
         colors: false
     }
     ,
+    end: false
+    ,
     variables: {}
     ,
     functions: {}
@@ -32,7 +34,6 @@ const curzs =
             if (command.startsWith('~/') && command.endsWith('/~')) {
                 return ''
             }
-
             else {
                 return `[CURZS:LINE:${this.line}]:['Invalid Command']`
             }
@@ -47,6 +48,100 @@ const curzs =
         if (tokens[0] == 'receive' && tokens[1] == '[colors.clib];') {
             this.packageEnabled.colors = true
             return `[CURZS:LINE:${this.line}]:['Package Added']`
+        }
+        if (tokens[0] == 'off' && tokens[1] == '[curzs];') {
+            this.end = true
+            return `[CURZS:LINE:${this.line}]:['Closed CURZS']`
+        }
+        if (tokens[0] == 'if' && tokens[1] && tokens[2] && tokens[3]) {
+            if (this.packageEnabled.basics == true) {
+                const unChecked = tokens[1].toString()
+                if (typeof(unChecked) == 'string') {
+                    if (unChecked.startsWith('[') && unChecked.endsWith(']')) {
+                        const a1 = unChecked.replace('[','').toString()
+                        const a2 = a1.replace(']','').toString()
+                        const condition = a2.split(',')
+                        try {
+                            const A = condition[0]
+                            const B = condition[1]
+                            const C = condition[2]
+                            if (B == '==') {
+                                if (A == C) {
+                                    const unChecked2 = tokens[2].toString()
+                                    const b1 = unChecked2.replace('[','').toString()
+                                    const b2 = b1.replace(']','').toString()
+                                    const b3 = b2.replace('_',' ').toString()
+                                    this.evaluator(this.parser(b3.split(' ')))
+                                } else {
+                                    const unChecked2 = tokens[3].toString()
+                                    const b1 = unChecked2.replace('[','').toString()
+                                    const b2 = b1.replace('];','').toString()
+                                    const b3 = b2.replace('_',' ').toString()
+                                    this.evaluator(this.parser(b3.split(' ')))
+                                }
+                            }
+                            if (B == '!=') {
+                                if (A != C) {
+                                    const unChecked2 = tokens[2].toString()
+                                    const b1 = unChecked2.replace('[','').toString()
+                                    const b2 = b1.replace(']','').toString()
+                                    const b3 = b2.replace('_',' ').toString()
+                                    this.evaluator(this.parser(b3.split(' ')))
+                                } else {
+                                    const unChecked2 = tokens[3].toString()
+                                    const b1 = unChecked2.replace('[','').toString()
+                                    const b2 = b1.replace('];','').toString()
+                                    const b3 = b2.replace('_',' ').toString()
+                                    this.evaluator(this.parser(b3.split(' ')))
+                                }
+                            }
+                            if (B == '>') {
+                                if (A == C) {
+                                    const unChecked2 = tokens[2].toString()
+                                    const b1 = unChecked2.replace('[','').toString()
+                                    const b2 = b1.replace(']','').toString()
+                                    const b3 = b2.replace('_',' ').toString()
+                                    this.evaluator(this.parser(b3.split(' ')))
+                                } else {
+                                    const unChecked2 = tokens[3].toString()
+                                    const b1 = unChecked2.replace('[','').toString()
+                                    const b2 = b1.replace('];','').toString()
+                                    const b3 = b2.replace('_',' ').toString()
+                                    this.evaluator(this.parser(b3.split(' ')))
+                                }
+                            }
+                            if (B == '<') {
+                                if (A == C) {
+                                    const unChecked2 = tokens[2].toString()
+                                    const b1 = unChecked2.replace('[','').toString()
+                                    const b2 = b1.replace(']','').toString()
+                                    const b3 = b2.replace('_',' ').toString()
+                                    this.evaluator(this.parser(b3.split(' ')))
+                                } else {
+                                    const unChecked2 = tokens[3].toString()
+                                    const b1 = unChecked2.replace('[','').toString()
+                                    const b2 = b1.replace('];','').toString()
+                                    const b3 = b2.replace('_',' ').toString()
+                                    this.evaluator(this.parser(b3.split(' ')))
+                                }
+                            }
+                        } catch (error) {
+                            return `[CURZS:LINE:${this.line}]:['Invalid Condition']`
+                        }
+                    }
+                }
+            } else {
+                return `[CURZS:LINE:${this.line}]:['Invalid Function']`
+            }
+        }
+        if (tokens[0] == 'once' && tokens[1] == '[js];') {
+            const input = nodeJs.prompt('$>')
+            try {
+                eval(input)
+                return `[CURZS:LINE:${this.line}]:['Finished JavaScript Command']`
+            } catch (error) {
+                return `[CURZS:LINE:${this.line}]:['Invalid JavaScript Command']`
+            }
         }
         if (tokens[0] == 'add' && tokens[1]) {
             if (this.packageEnabled.basics == true) {
@@ -152,7 +247,11 @@ const curzs =
         this.line++
         const input = nodeJs.prompt('%>')
         this.singleLine(input)
-        this.interpreter()
+        if (this.end == false) {
+            this.interpreter()
+        } else {
+            return
+        }
     }
     ,
     manual(file) {
